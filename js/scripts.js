6a03141c41c282b3b2da1d242c5556d08a6f6663
Stewart ilondanga <stilondanga@gmail.com>
2019-02-16 12:02:18 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -100,34 +100,34 @@ function slide(wrapper, items, prev, next) {
       if (!action) {
         posInitial = items.offsetLeft;
       }
-      /*
-                                              if (dir == 1) {
-                                                items.style.left = (posInitial - slideSize) + "px";
-                                                index++;
-                                              } else if (dir == -1) {
-                                                items.style.left = (posInitial + slideSize) + "px";
-                                                index--;
+
+      if (dir == 1) {
+        items.style.left = (posInitial - slideSize) + "px";
+        index++;
+      } else if (dir == -1) {
+        items.style.left = (posInitial + slideSize) + "px";
+        index--;
+      }
+    };
+    /*
+                                                allowShift = false;
                                               }
-                                            };
 
-                                            allowShift = false;
-                                          }
+                                              function checkIndex (){
+                                                items.classList.remove('shifting');
 
-                                          function checkIndex (){
-                                            items.classList.remove('shifting');
+                                                if (index == -1) {
+                                                  items.style.left = -(slidesLength * slideSize) + "px";
+                                                  index = slidesLength - 1;
+                                                }
 
-                                            if (index == -1) {
-                                              items.style.left = -(slidesLength * slideSize) + "px";
-                                              index = slidesLength - 1;
+                                                if (index == slidesLength) {
+                                                  items.style.left = -(1 * slideSize) + "px";
+                                                  index = 0;
+                                                }
+
+                                                allowShift = true;
+                                              }
                                             }
 
-                                            if (index == slidesLength) {
-                                              items.style.left = -(1 * slideSize) + "px";
-                                              index = 0;
-                                            }
-
-                                            allowShift = true;
-                                          }
-                                        }
-
-                                        slide(slider, sliderItems, prev, next);
+                                            slide(slider, sliderItems, prev, next);
