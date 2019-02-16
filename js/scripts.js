3c5238058f560209aafee5a57b26c4f29ec4e307
Stewart ilondanga <stilondanga@gmail.com>
2019-02-16 12:03:33 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -112,22 +112,22 @@ function slide(wrapper, items, prev, next) {
 
     allowShift = false;
   }
-  /*
-                                                function checkIndex (){
-                                                  items.classList.remove('shifting');
 
-                                                  if (index == -1) {
-                                                    items.style.left = -(slidesLength * slideSize) + "px";
-                                                    index = slidesLength - 1;
+  function checkIndex() {
+    items.classList.remove('shifting');
+    /*
+                                                      if (index == -1) {
+                                                        items.style.left = -(slidesLength * slideSize) + "px";
+                                                        index = slidesLength - 1;
+                                                      }
+
+                                                      if (index == slidesLength) {
+                                                        items.style.left = -(1 * slideSize) + "px";
+                                                        index = 0;
+                                                      }
+
+                                                      allowShift = true;
+                                                    }
                                                   }
 
-                                                  if (index == slidesLength) {
-                                                    items.style.left = -(1 * slideSize) + "px";
-                                                    index = 0;
-                                                  }
-
-                                                  allowShift = true;
-                                                }
-                                              }
-
-                                              slide(slider, sliderItems, prev, next);
+                                                  slide(slider, sliderItems, prev, next);
