372a41f68ac35d161b90252ac4c8a3aee3e3302b
Stewart ilondanga <stilondanga@gmail.com>
2019-02-16 11:56:04 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -40,88 +40,92 @@ function slide(wrapper, items, prev, next) {
   items.addEventListener('touchstart', dragStart);
   items.addEventListener('touchend', dragEnd);
   items.addEventListener('touchmove', dragAction);
+
+  // Click events
+  prev.addEventListener('click', function() {
+    shiftSlide(-1)
+  });
+  next.addEventListener('click', function() {
+    shiftSlide(1)
+  });
   /*
-          // Click events
-          prev.addEventListener('click', function () { shiftSlide(-1) });
-          next.addEventListener('click', function () { shiftSlide(1) });
+            // Transition events
+            items.addEventListener('transitionend', checkIndex);
 
-          // Transition events
-          items.addEventListener('transitionend', checkIndex);
+            function dragStart (e) {
+              e = e || window.event;
+              e.preventDefault();
+              posInitial = items.offsetLeft;
 
-          function dragStart (e) {
-            e = e || window.event;
-            e.preventDefault();
-            posInitial = items.offsetLeft;
-
-            if (e.type == 'touchstart') {
-              posX1 = e.touches[0].clientX;
-            } else {
-              posX1 = e.clientX;
-              document.onmouseup = dragEnd;
-              document.onmousemove = dragAction;
-            }
-          }
-
-          function dragAction (e) {
-            e = e || window.event;
-
-            if (e.type == 'touchmove') {
-              posX2 = posX1 - e.touches[0].clientX;
-              posX1 = e.touches[0].clientX;
-            } else {
-              posX2 = posX1 - e.clientX;
-              posX1 = e.clientX;
-            }
-            items.style.left = (items.offsetLeft - posX2) + "px";
-          }
-
-          function dragEnd (e) {
-            posFinal = items.offsetLeft;
-            if (posFinal - posInitial < -threshold) {
-              shiftSlide(1, 'drag');
-            } else if (posFinal - posInitial > threshold) {
-              shiftSlide(-1, 'drag');
-            } else {
-              items.style.left = (posInitial) + "px";
-            }
-
-            document.onmouseup = null;
-            document.onmousemove = null;
-          }
-
-          function shiftSlide(dir, action) {
-            items.classList.add('shifting');
-
-            if (allowShift) {
-              if (!action) { posInitial = items.offsetLeft; }
-
-              if (dir == 1) {
-                items.style.left = (posInitial - slideSize) + "px";
-                index++;
-              } else if (dir == -1) {
-                items.style.left = (posInitial + slideSize) + "px";
-                index--;
+              if (e.type == 'touchstart') {
+                posX1 = e.touches[0].clientX;
+              } else {
+                posX1 = e.clientX;
+                document.onmouseup = dragEnd;
+                document.onmousemove = dragAction;
               }
-            };
-
-            allowShift = false;
-          }
-
-          function checkIndex (){
-            items.classList.remove('shifting');
-
-            if (index == -1) {
-              items.style.left = -(slidesLength * slideSize) + "px";
-              index = slidesLength - 1;
             }
 
-            if (index == slidesLength) {
-              items.style.left = -(1 * slideSize) + "px";
-              index = 0;
+            function dragAction (e) {
+              e = e || window.event;
+
+              if (e.type == 'touchmove') {
+                posX2 = posX1 - e.touches[0].clientX;
+                posX1 = e.touches[0].clientX;
+              } else {
+                posX2 = posX1 - e.clientX;
+                posX1 = e.clientX;
+              }
+              items.style.left = (items.offsetLeft - posX2) + "px";
             }
 
-            allowShift = true;
-          }
-        }
+            function dragEnd (e) {
+              posFinal = items.offsetLeft;
+              if (posFinal - posInitial < -threshold) {
+                shiftSlide(1, 'drag');
+              } else if (posFinal - posInitial > threshold) {
+                shiftSlide(-1, 'drag');
+              } else {
+                items.style.left = (posInitial) + "px";
+              }
 
-        slide(slider, sliderItems, prev, next);
+              document.onmouseup = null;
+              document.onmousemove = null;
+            }
+
+            function shiftSlide(dir, action) {
+              items.classList.add('shifting');
+
+              if (allowShift) {
+                if (!action) { posInitial = items.offsetLeft; }
+
+                if (dir == 1) {
+                  items.style.left = (posInitial - slideSize) + "px";
+                  index++;
+                } else if (dir == -1) {
+                  items.style.left = (posInitial + slideSize) + "px";
+                  index--;
+                }
+              };
+
+              allowShift = false;
+            }
+
+            function checkIndex (){
+              items.classList.remove('shifting');
+
+              if (index == -1) {
+                items.style.left = -(slidesLength * slideSize) + "px";
+                index = slidesLength - 1;
+              }
+
+              if (index == slidesLength) {
+                items.style.left = -(1 * slideSize) + "px";
+                index = 0;
+              }
+
+              allowShift = true;
+            }
+          }
+
+          slide(slider, sliderItems, prev, next);
