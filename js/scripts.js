1185dc8395d5fbba492f1d5ff4a0fdf3b9074a3d
Stewart ilondanga <stilondanga@gmail.com>
2019-02-16 12:01:42 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -95,37 +95,39 @@ function slide(wrapper, items, prev, next) {
 
   function shiftSlide(dir, action) {
     items.classList.add('shifting');
-    /*
-                                      if (allowShift) {
-                                        if (!action) { posInitial = items.offsetLeft; }
 
-                                        if (dir == 1) {
-                                          items.style.left = (posInitial - slideSize) + "px";
-                                          index++;
-                                        } else if (dir == -1) {
-                                          items.style.left = (posInitial + slideSize) + "px";
-                                          index--;
+    if (allowShift) {
+      if (!action) {
+        posInitial = items.offsetLeft;
+      }
+      /*
+                                              if (dir == 1) {
+                                                items.style.left = (posInitial - slideSize) + "px";
+                                                index++;
+                                              } else if (dir == -1) {
+                                                items.style.left = (posInitial + slideSize) + "px";
+                                                index--;
+                                              }
+                                            };
+
+                                            allowShift = false;
+                                          }
+
+                                          function checkIndex (){
+                                            items.classList.remove('shifting');
+
+                                            if (index == -1) {
+                                              items.style.left = -(slidesLength * slideSize) + "px";
+                                              index = slidesLength - 1;
+                                            }
+
+                                            if (index == slidesLength) {
+                                              items.style.left = -(1 * slideSize) + "px";
+                                              index = 0;
+                                            }
+
+                                            allowShift = true;
+                                          }
                                         }
-                                      };
 
-                                      allowShift = false;
-                                    }
-
-                                    function checkIndex (){
-                                      items.classList.remove('shifting');
-
-                                      if (index == -1) {
-                                        items.style.left = -(slidesLength * slideSize) + "px";
-                                        index = slidesLength - 1;
-                                      }
-
-                                      if (index == slidesLength) {
-                                        items.style.left = -(1 * slideSize) + "px";
-                                        index = 0;
-                                      }
-
-                                      allowShift = true;
-                                    }
-                                  }
-
-                                  slide(slider, sliderItems, prev, next);
+                                        slide(slider, sliderItems, prev, next);
